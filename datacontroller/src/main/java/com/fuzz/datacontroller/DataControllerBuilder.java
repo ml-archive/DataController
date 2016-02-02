1f928b03883f958453249c0c602cef89d4216537
fuzzagrosner <andrew@fuzzproductions.com>
2016-02-02 16:09:34 -0500

    added datacontroller builder class

---
+++ datacontroller/src/main/java/com/fuzz/datacontroller/DataControllerBuilder.java
@@ -0,0 +1,57 @@
+package com.fuzz.datacontroller;
+
+import com.fuzz.datacontroller.fetcher.DataFetcher;
+import com.fuzz.datacontroller.strategy.IRefreshStrategy;
+
+/**
+ * Description: A builder that helpfully constructs a custom {@link DataController} without need
+ * to explicitly subclass it.
+ */
+public class DataControllerBuilder<TResponse> {
+
+    /**
+     * Simple interface for checking if empty.
+     *
+     * @param <TResponse>
+     */
+    public interface IEmpty<TResponse> {
+
+        /**
+         * @param response The response to validate.
+         * @return True if the response is deemed empty here.
+         */
+        boolean isEmpty(TResponse response);
+    }
+
+    private DataFetcher<TResponse> dataFetcher;
+    private IRefreshStrategy refreshStrategy;
+
+    public DataControllerBuilder<TResponse> setDataFetcher(DataFetcher<TResponse> dataFetcher) {
+        this.dataFetcher = dataFetcher;
+        return this;
+    }
+
+    public DataControllerBuilder<TResponse> setRefreshStrategy(IRefreshStrategy refreshStrategy) {
+        this.refreshStrategy = refreshStrategy;
+        return this;
+    }
+
+    public DataController<TResponse> build(final IEmpty<TResponse> emptyMethod) {
+        DataController<TResponse> dataController = new DataController<TResponse>() {
+            @Override
+            protected boolean isEmpty(TResponse tResponse) {
+                return emptyMethod != null && emptyMethod.isEmpty(tResponse);
+            }
+
+            @Override
+            protected DataFetcher<TResponse> createDataFetcher() {
+                return dataFetcher;
+            }
+        };
+        if (refreshStrategy != null) {
+            dataController.setRefreshStrategy(refreshStrategy);
+        }
+        return dataController;
+    }
+
+}
