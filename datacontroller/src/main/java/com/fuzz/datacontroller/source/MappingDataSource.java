448cbb26f3837244c1124df61e2acb6468fddb51
fuzzagrosner <andrew@fuzzproductions.com>
2016-06-21 09:09:58 -0400

    added mapping data source, which maps the type of one source to another.

---
+++ datacontroller/src/main/java/com/fuzz/datacontroller/source/MappingDataSource.java
@@ -0,0 +1,54 @@
+package com.fuzz.datacontroller.source;
+
+import com.fuzz.datacontroller.DataController;
+import com.fuzz.datacontroller.DataControllerResponse;
+
+/**
+ * Description: Maps the callbacks of one source with another type.
+ */
+public class MappingDataSource<TFromResponse, TResponse> extends DataSource<TResponse> {
+
+    public interface Mapper<TFromResponse, TResponse> {
+
+        TResponse mapFrom(TFromResponse fromResponse);
+
+        TFromResponse mapTo(TResponse response);
+    }
+
+    private final DataSource<TFromResponse> fromDataSource;
+    private final Mapper<TFromResponse, TResponse> mapper;
+
+    public MappingDataSource(DataSource<TFromResponse> fromDataSource,
+                             Mapper<TFromResponse, TResponse> mapper) {
+        this.fromDataSource = fromDataSource;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public void cancel() {
+        fromDataSource.cancel();
+    }
+
+    @Override
+    protected void doGet(SourceParams sourceParams, final DataController.Success<TResponse> success, DataController.Error error) {
+        fromDataSource.doGet(sourceParams, new DataController.Success<TFromResponse>() {
+            @Override
+            public void onSuccess(DataControllerResponse<TFromResponse> response) {
+                success.onSuccess(new DataControllerResponse<>(mapper.mapFrom(response.getResponse()),
+                        response.getSourceType(), response.getOriginalUrl()));
+            }
+        }, error);
+    }
+
+    @Override
+    protected void doStore(DataControllerResponse<TResponse> response) {
+        fromDataSource.doStore(new DataControllerResponse<>(mapper.mapTo(response.getResponse()),
+                response.getSourceType(), response.getOriginalUrl()));
+    }
+
+    @Override
+    public SourceType getSourceType() {
+        return fromDataSource.getSourceType();
+    }
+
+}
